7201abdc587dc5e5cc3796134f344602a6ea2a21
wantao <13813872403>
2022-12-13 17:24:41 +0800

    '打包配置'

--- src/views/doc/swiper/demo1.tsx
+++ src/views/doc/swiper/demo1.tsx
@@ -3,49 +3,49 @@ export default defineComponent({
     props: {},
     setup(props, context) {
         const style = {
-            height:'20vh',
-            textAlign:'center'
+            height: '20vh',
+            textAlign: 'center'
         }
         return () => (
             <>
                 <demo-block title='基础用法'>
                     <want-swiper>
-                        <want-swiper-item style={{background:'lightblue',...style}}>1</want-swiper-item>
-                        <want-swiper-item style={{background:'lightcoral',...style}}>2</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgreen',...style}}>3</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgoldenrodyellow',...style}}>4</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightblue', ...style }}>1</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightcoral', ...style }}>2</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgreen', ...style }}>3</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgoldenrodyellow', ...style }}>4</want-swiper-item>
                     </want-swiper>
                 </demo-block>
                 <demo-block title='循环'>
-                    <want-swiper loop  activeIndex={3}>
-                        <want-swiper-item style={{background:'lightblue',...style}}>1</want-swiper-item>
-                        <want-swiper-item style={{background:'lightcoral',...style}}>2</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgreen',...style}}>3</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgoldenrodyellow',...style}}>4</want-swiper-item>
+                    <want-swiper loop activeIndex={3}>
+                        <want-swiper-item style={{ background: 'lightblue', ...style }}>1</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightcoral', ...style }}>2</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgreen', ...style }}>3</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgoldenrodyellow', ...style }}>4</want-swiper-item>
                     </want-swiper>
                 </demo-block>
-                <demo-block title='自动播放'>
+                <demo-block title='自动播放' loop>
                     <want-swiper autoplay>
-                        <want-swiper-item style={{background:'lightblue',...style}}>1</want-swiper-item>
-                        <want-swiper-item style={{background:'lightcoral',...style}}>2</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgreen',...style}}>3</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgoldenrodyellow',...style}}>4</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightblue', ...style }}>1</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightcoral', ...style }}>2</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgreen', ...style }}>3</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgoldenrodyellow', ...style }}>4</want-swiper-item>
                     </want-swiper>
                 </demo-block>
                 <demo-block title='指示器在外部显示'>
                     <want-swiper dotOutside>
-                        <want-swiper-item style={{background:'lightblue',...style}}>1</want-swiper-item>
-                        <want-swiper-item style={{background:'lightcoral',...style}}>2</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgreen',...style}}>3</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgoldenrodyellow',...style}}>4</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightblue', ...style }}>1</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightcoral', ...style }}>2</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgreen', ...style }}>3</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgoldenrodyellow', ...style }}>4</want-swiper-item>
                     </want-swiper>
                 </demo-block>
                 <demo-block title='隐藏指示器'>
                     <want-swiper indicator={false}>
-                        <want-swiper-item style={{background:'lightblue',...style}}>1</want-swiper-item>
-                        <want-swiper-item style={{background:'lightcoral',...style}}>2</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgreen',...style}}>3</want-swiper-item>
-                        <want-swiper-item style={{background:'lightgoldenrodyellow',...style}}>4</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightblue', ...style }}>1</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightcoral', ...style }}>2</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgreen', ...style }}>3</want-swiper-item>
+                        <want-swiper-item style={{ background: 'lightgoldenrodyellow', ...style }}>4</want-swiper-item>
                     </want-swiper>
                 </demo-block>
             </>
